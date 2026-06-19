08c254ef18b89718f325c9d5d44fb1666df0caa1
HectorTH <hectormanueltoralhuerta@gmail.com>
2026-06-19 15:31:57 -0600

    Update Salones.kt

--- InfoAula/app/src/main/java/com/fei/infoaula/Salones.kt
+++ InfoAula/app/src/main/java/com/fei/infoaula/Salones.kt
@@ -17,4 +17,14 @@ class Salones : AppCompatActivity() {
             insets
         }
     }
+    val listaSalones = listOf(
+        Salon(101, EstadoSalon.DISPONIBLE, horaDisponible = "14:00"),
+        Salon(102, EstadoSalon.OCUPADO, horaOcupado = "15:30"),
+        Salon(103, EstadoSalon.RESERVADO, horaReserva = "16:00"),
+        Salon(104, EstadoSalon.DISPONIBLE, horaDisponible = "13:45"),
+        Salon(105, EstadoSalon.OCUPADO, horaOcupado = "17:00"),
+        Salon(106, EstadoSalon.RESERVADO, horaReserva = "18:00"),
+        Salon(107, EstadoSalon.DISPONIBLE, horaDisponible = "12:30"),
+        Salon(108, EstadoSalon.OCUPADO, horaOcupado = "14:45")
+    )
 }
