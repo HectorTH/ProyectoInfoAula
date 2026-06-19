6cc18d7650cb930f06fca10452d1158aa4dae2c8
HectorTH <hectormanueltoralhuerta@gmail.com>
2026-06-19 16:22:58 -0600

    Update Salones.kt

--- InfoAula/app/src/main/java/com/fei/infoaula/Salones.kt
+++ InfoAula/app/src/main/java/com/fei/infoaula/Salones.kt
@@ -6,17 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class Salones : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_salones)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+    //Lista de salones
     val listaSalones = listOf(
         Salon(101, EstadoSalon.DISPONIBLE, horaDisponible = "14:00"),
         Salon(102, EstadoSalon.OCUPADO, horaOcupado = "15:30"),
@@ -27,4 +19,20 @@ class Salones : AppCompatActivity() {
         Salon(107, EstadoSalon.DISPONIBLE, horaDisponible = "12:30"),
         Salon(108, EstadoSalon.OCUPADO, horaOcupado = "14:45")
     )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_salones)
+        //Referenciar con la ListView
+        val listView = findViewById<ListView>(R.id.lvDatos)
+        //conectar la lista con el adapter
+        val adapter = SalonesAdapter(this,listaSalones)
+        listView.adapter = adapter
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 }
